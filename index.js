f6914201b2140f3425bf23f14f3aa47df7900367
bonbongiraffe <francescowai@yahoo.com>
2023-05-12 07:16:53 -0400

    complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+//create header element and populate according to specifications
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id","victory");
+newHeader.textContent = "Frankie is the champion";
+//fetch main element and remove id 'main'
+const main = document.getElementById("main");
+main.removeAttribute("id","main");
