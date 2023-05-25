94592a8402f4fe61e419e738dad45b70dea0c847
bonbongiraffe <francescowai@yahoo.com>
2023-05-25 09:19:02 -0400

    clean slate for pairing lab

--- index.js
+++
@@ -1,7 +0,0 @@
-//create header element and populate according to specifications
-const newHeader = document.createElement("h1");
-newHeader.setAttribute("id","victory");
-newHeader.textContent = "Frankie is the champion";
-//fetch main element and remove id 'main'
-const main = document.getElementById("main");
-main.removeAttribute("id","main");
